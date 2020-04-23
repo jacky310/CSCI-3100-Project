06fa4663f6f8de9e56abb6908a7f1b9417bbca3e
JackWong0303 <48310199+JackWong0303@users.noreply.github.com>
2020-04-24 00:53:01 +0800

    Resolve conflict

--- Server/website/controller/uploadPhotoMiddleware.js
+++ Server/website/controller/uploadPhotoMiddleware.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // const util = require("util");
 // const multer = require("multer");
 // const GridFsStorage = require("multer-gridfs-storage");
@@ -52,58 +51,3 @@ var uploadFile = multer({ storage: storage }).array("file", 3);
 var uploadFilesMiddleware = util.promisify(uploadFile);
 
 module.exports = uploadFilesMiddleware;
-=======
-// const util = require("util");
-// const multer = require("multer");
-// const GridFsStorage = require("multer-gridfs-storage");
-//
-// var storage = new GridFsStorage({
-//   url: "mongodb+srv://jacky:jacky310@cluster0-5jjxe.gcp.mongodb.net/PartyRoomBooking?retryWrites=true&w=majority",
-//   options: { useNewUrlParser: true, useUnifiedTopology: true },
-//   file: (req, file) => {
-//     const match = ["image/png", "image/jpg"];
-//
-//     if (match.indexOf(file.mimetype) === -1) {
-//       const photeName = `123-${file.originalname}`;
-//       return photoName;
-//     }
-//
-//     return {
-//       bucketName: "photos",
-//       filename: `123-${file.originalname}`
-//     };
-//   }
-// });
-//
-// var uploadPhoto = multer({ storage: storage }).single('photo1');
-// var uploadPhotoMiddleware = util.promisify(uploadPhoto);
-//
-// module.exports = uploadPhotoMiddleware;
-
-const util = require("util");
-const multer = require("multer");
-const GridFsStorage = require("multer-gridfs-storage");
-
-var storage = new GridFsStorage({
-  url: "mongodb+srv://jacky:jacky310@cluster0-5jjxe.gcp.mongodb.net/PartyRoomBooking?retryWrites=true&w=majority",
-  options: { useNewUrlParser: true, useUnifiedTopology: true },
-  file: (req, file) => {
-    const match = ["image/png", "image/jpeg"];
-
-    if (match.indexOf(file.mimetype) === -1) {
-      const photeName = `${file.originalname}`;
-      return photoName;
-    }
-
-    return {
-      bucketName: "photos",
-      filename: `${file.originalname}`
-    };
-  }
-});
-
-var uploadFile = multer({ storage: storage }).array("file", 3);
-var uploadFilesMiddleware = util.promisify(uploadFile);
-
-module.exports = uploadFilesMiddleware;
->>>>>>> 5f5296113ae93e1908b81f0ae5d84f88a8b7ca4a
